30f68905c071a2ba30a3bf066fc2c8cd7d867fab
Aaron Carlino <unclecheese@leftandmain.com>
2019-09-02 22:58:28 +1200

    Configure public-files

--- public/install.php
+++ public/install.php
@@ -4,22 +4,15 @@
  ************************************************************************************
  **                                                                                **
  **  If you can read this text in your browser then you don't have PHP installed.  **
- **  Please install PHP 7.1.0 or higher.                                           **
+ **  Please install PHP 5.6.0 or higher.                                           **
  **                                                                                **
  ************************************************************************************
  ************************************************************************************/
 
-/**
- * PHP version check. Make sure we've got at least PHP 7.1.0 in the most friendly way possible
- */
-if (version_compare(phpversion(), '7.1.0', '<')) {
-    header($_SERVER['SERVER_PROTOCOL'] . " 500 Server Error");
-    echo str_replace(
-        '$PHPVersion',
-        phpversion(),
-        file_get_contents(__DIR__ . "/php7-required.html")
-    );
-    die();
+if (file_exists(__DIR__ . '/vendor/silverstripe/installer-wizard/src/install.php')) {
+    include __DIR__ . '/vendor/silverstripe/installer-wizard/src/install.php';
+} elseif (file_exists(__DIR__ . '/../vendor/silverstripe/installer-wizard/install.php')) {
+    include __DIR__ . '/../vendor/silverstripe/installer-wizard/src/install.php';
+} else {
+    include 'install-frameworkmissing.html';
 }
-
-include(__DIR__ . '/install7.php');
